eff7392a257a7b175c344cbf6b2fde98aa62de4e
rosemariekleinschmidt <rosemarie.santos@escola.pr.gov.br>
2025-03-25 17:40:11 +0000

    .

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let tamanhoAtualFonte = 1;
-
-    const aumentaFonteBotao = document.getElementById(aumentar-fonte);
-    const diminuiFonteBotão= document.getElementById(diminuir-fonte);
-    aumentaFonteBotao.addEventListener('click', function(){;
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fonteSize= ${tamanhoAtualFonte}rem
+document.addEventListener('DOMContentLoaded', function() {
+    let tamanhoAtualFonte=1;
+    const aumentaFonteBotao=document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao=document.getElementById('diminuir-fonte');
+aumentaFonteBotao.addEventListener('click', function(){;
+        tamanhoAtualFonte +=0.1;
+        document.body.style.fontSize=${tamanhoAtualFonte}rem
     })
-   
-   
-    const diminuiFonteBotão= document.getElementById(diminuir-fonte);
-    diminuiFonteBotao.addEventListener('click', function(){;
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fonteSize= ${tamanhoAtualFonte}rem
-})
+diminuiFonteBotao.addEventListener('click', function(){;
+        tamanhoAtualFonte -=0.1;
+        document.body.style.fontSize=${tamanhoAtualFonte}rem
+    })
+
+
 })

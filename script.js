d121a3f1f38ddfbe48e6e9b0da63e0e7d1006529
rosemariekleinschmidt <rosemarie.santos@escola.pr.gov.br>
2025-03-25 17:34:20 +0000

    inserção dos comandos de diminui botão

--- script.js
+++ script.js
@@ -2,28 +2,16 @@ document.addEventListener('DOMContentLoaded', function(){
     let tamanhoAtualFonte = 1;
 
     const aumentaFonteBotao = document.getElementById(aumentar-fonte);
+    const diminuiFonteBotão= document.getElementById(diminuir-fonte);
     aumentaFonteBotao.addEventListener('click', function(){;
     tamanhoAtualFonte += 0.1;
     document.body.style.fonteSize= ${tamanhoAtualFonte}rem
     })
+   
+   
+    const diminuiFonteBotão= document.getElementById(diminuir-fonte);
+    diminuiFonteBotao.addEventListener('click', function(){;
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fonteSize= ${tamanhoAtualFonte}rem
 })
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function(){
-
-    let tamanhoAtualFonte = 1;
-
-    const aumentaFonteBotao = document.getElementById(aumentar-fonte);
-    aumentaFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fonteSize= ${tamanhoAtualFonte}rem
-    })
 })

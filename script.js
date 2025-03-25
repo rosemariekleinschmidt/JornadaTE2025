8f87d7d8967854977f71fcd9df62b2c08e924a79
rosemariekleinschmidt <rosemarie.santos@escola.pr.gov.br>
2025-03-25 17:24:33 +0000

    .

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
+    let tamanhoAtualFonte = 1;
+
+    const aumentaFonteBotao = document.getElementById(aumentar-fonte);
+    aumentaFonteBotao.addEventListener('click', function(){;
+    tamanhoAtualFonte += 0.1;
+    document.body.style.fonteSize= ${tamanhoAtualFonte}rem
+    })
+})
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
 
     let tamanhoAtualFonte = 1;
 

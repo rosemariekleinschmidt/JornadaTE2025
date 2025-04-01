dbe8c387babcf8f4b57799f8306119eb526eb603
rosemariekleinschmidt <rosemarie.santos@escola.pr.gov.br>
2025-04-01 18:04:23 +0000

    .

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+
+    })
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+})
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     let tamanhoAtualFonte=1;
     const aumentarFonteBotao=document.getElementById('aumentar-fonte');
     const diminuirFonteBotao=document.getElementById('diminuir-fonte');
-aumentarFonteBotao.addEventListener('click', function(){
+aumentarFonteBotao.addEventListener('click', function(){;
         tamanhoAtualFonte +=0.1;
         document.body.style.fontSize=${tamanhoAtualFonte}rem
     })
-diminuirFonteBotao.addEventListener('click', function(){
+diminuirFonteBotao.addEventListener('click', function(){;
         tamanhoAtualFonte -=0.1;
         document.body.style.fontSize=${tamanhoAtualFonte}rem
     })
-    document.addEventListener ('DOMContentLoaded', function(){
-        const botaodeAcessibilidade = document.getElemntBuId ('botao-acessibilidade');
-        const opcoesdeAcessibilidade = document.getElementById ('opcoes-acessibilidade');
-        
-        botaodeAcessibilidade.addeventListener("click", function(){
-            botaodeAcessibilidade.classList.toggle('rotacao-botao');
-            opcoesdeAcessibilidade.classList.toggle('apresenta-lista');
-        })
-    })
+
 
 })

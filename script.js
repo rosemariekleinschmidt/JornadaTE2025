a4effcb5eb0d41fd682ab246a0217917b1bd01be
rosemariekleinschmidt <rosemarie.santos@escola.pr.gov.br>
2025-04-01 18:13:09 +0000

    .

--- script.js
+++ script.js
@@ -11,6 +11,17 @@ diminuirFonteBotao.addEventListener('click', function(){;
         tamanhoAtualFonte -=0.1;
         document.body.style.fontSize=${tamanhoAtualFonte}rem
     })
-
+    .opcoes-acessibilidade{
+        margin-top:10px;
+        display: flex;
+        flex-direction: column;
+    }
+    
+    .opcoes-acessibilidade button{
+        margin-bottom: 5px;
+    }
+    .apresenta-lista{
+        display: none;
+    }
 
 })

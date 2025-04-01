0f627dfa64a63bbfae797ab93c0463eaf8272b73
rosemariekleinschmidt <rosemarie.santos@escola.pr.gov.br>
2025-04-01 17:58:05 +0000

    adição de códigos Script.js

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     let tamanhoAtualFonte=1;
     const aumentarFonteBotao=document.getElementById('aumentar-fonte');
     const diminuirFonteBotao=document.getElementById('diminuir-fonte');
-aumentarFonteBotao.addEventListener('click', function(){;
+aumentarFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte +=0.1;
         document.body.style.fontSize=${tamanhoAtualFonte}rem
     })
-diminuirFonteBotao.addEventListener('click', function(){;
+diminuirFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -=0.1;
         document.body.style.fontSize=${tamanhoAtualFonte}rem
     })
-
+    document.addEventListener ('DOMContentLoaded', function(){
+        const botaode|Acessibilidade = document.getElemntBuId ('botao-acessibilidade');
+        const opcoesdeAcessibilidade = document.getElementById ('opcoes-acessibilidade');
+        
+        botaodeAcessibilidade.addeventListener("click", function(){
+            botaodeAcessibilidade.classList.toggle('rotacao-botao');
+            opcoesdeAcessibilidade.classList.toggle('apresenta-lista');
+        })
+    })
 
 })

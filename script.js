f8068e9718bdee6f39fc5f0c1e62c88b8eb25ae1
rosemariekleinschmidt <rosemarie.santos@escola.pr.gov.br>
2025-04-08 14:51:28 +0000

    .

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function (){
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
-    botaoDeAcessibilidade.addEventListener('click', function () {
+    botaoDeAcessibilidade.addEventListener('click', function (){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
     })
-
+    
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const diminuiFonteBotao = document.getElementById('dimuir-fonte');
 
-    const alternaContraste = document.getElementById('alterna-contraste')
+    const alternaContraste = this.documentElement.getElementById ('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
-
-    aumentaFonteBotao.addEventListener('click', function () {
+   
+    aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
     })
-
-    diminuiFonteBotao.addEventListener('click', function () {
+    diminuiFonteBotao.addEventListener('click'), function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
     })
-
-    alternaContraste.addEventListener('click', function () {
-        document.body.classList.toggle('alto-contraste')
+    alternaContraste.addEventListener('click', function (){
+        document.body.classList.toogle('alto-contraste')
     })
-
 
 })
+
